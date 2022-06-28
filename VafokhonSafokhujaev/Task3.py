a752cc026975494dfc4df9c50f63ef0c5353f6d4
Vafo <vafo2002@gmail.com>
2022-06-28 06:12:46 +0500

    Added Task4.2 solution

--- VafokhonSafokhujaev/Task3.py
+++ VafokhonSafokhujaev/Task3.py
@@ -16,7 +16,33 @@ def get_top_performers(filepath, number_of_students: int = 5):
 
     return res
 
+def sort_by_age(file_src, file_out):
+    entries = []
+    model = ""
+
+    with open(file_src, "r") as file:
+        model = file.readline().rstrip().split(',')
+        
+        for line in file:
+            data = line.rstrip().split(',')
+            entry = { key:val for key, val in zip(model, data)  }
+            entries.append(entry)
+
+    entries = sorted(entries, key = lambda dic : float(dic["age"]), reverse=True)
+
+    with open(file_out, "w") as file:
+        file.write(",".join(model) + "\n")
+
+        for entry in entries:
+
+            file.write(",".join( [val for key, val in entry.items()] ) + "\n")
+
+
+    return file.closed
+
 
 losers = get_top_performers("../data/students.csv")
 print(losers)
+
+sort_by_age("../data/students.csv", "../data/students_srt_by_age.csv")
 
